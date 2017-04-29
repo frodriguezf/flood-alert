4df82b051dd7c864cf0bc4d5d857fa1e55dc1d29
Franco <frodriguez@ascentio.com.ar>
2017-04-29 20:33:03 -0300

    Add SQLAlchemy database access with models and serializers

--- python-api/models/alert.py
+++ python-api/models/alert.py
@@ -1,7 +1,13 @@
-from app import db
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 
 class Alert(db.Model):
+    WARNING = 'WARNING'
+    CAUTION = 'CAUTION'
+    DANGER = 'DANGER'
+
     id = db.Column(db.Integer, primary_key=True, unique=True)
     latitude = db.Column(db.Float)
     longitude = db.Column(db.Float)
